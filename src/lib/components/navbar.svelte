ae3fcb5f9d0e4d9d4ccf7331c2a966a484a87707
Alish Giri <alishgiri.92@gmail.com>
2024-08-18 14:14:45 +0545

    Fix navbar responsiveness

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,12 +2,10 @@
 	import SuperCalendarLogo from '$lib/icons/super-calendar-logo.svelte';
 </script>
 
-<nav class="flex flex-row items-center py-5 md:p-3">
+<nav class="flex flex-row items-center p-3 py-5">
 	<a href="/" class="flex flex-row">
 		<SuperCalendarLogo />
-		<p class="ml-2 hidden text-2xl font-black tracking-wider text-app-dark md:flex">
-			Super Calendar
-		</p>
+		<p class="ml-2 text-2xl font-black tracking-wider text-app-dark">Super Calendar</p>
 	</a>
 	<div class="flex flex-grow"></div>
 	<div class="mx-4 flex flex-col items-end"></div>
