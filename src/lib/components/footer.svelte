6e4dee141da5106bd02ccbdb299db8310ef0ebb7
Alish Giri <alishgiri.92@gmail.com>
2024-08-18 14:02:26 +0545

    Add navbar and footer.

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<footer class="absolute bottom-0 right-0 flex flex-row items-center px-2">
+	<div class="flex flex-grow" />
+	<p class="m-2 self-end text-sm">&copy; 2024 Super Calendar</p>
+</footer>
